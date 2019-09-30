8e6f1c0c318f35f151fb63f40c879e71af4adbc7
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-09-30 15:28:51 -0600

    Add obj to card class

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ class Card {
   constructor(obj) {
     this.id = obj.id;
     this.question = obj.question;
-    this.answers  
-    this.correctAnswer
+    this.answers = obj.answers;  
+    this.correctAnswer = obj.correctAnswer;
   }
 
 }
